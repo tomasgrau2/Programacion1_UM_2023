030e78dc6bff9f9d0d7dd0121973cacaf361d2fd
tomasgrau2 <t.grau@alumno.um.edu.ar>
2023-04-10 21:15:32 -0300

    TP2 sin roles

--- backend/main/__init__.py
+++ backend/main/__init__.py
@@ -22,6 +22,28 @@ def create_app():
     #cargar a la API el recurso Animal y especificar la ruta 
     api.add_resource(resources.AnimalResource, '/animal/<id>')
 
+    api.add_resource(resources.UsuariosResource, '/usuarios')
+
+    api.add_resource(resources.UsuarioResource, '/usuario/<id>')
+
+    api.add_resource(resources.UsuariosAlumnosResource, '/alumnos')
+
+    api.add_resource(resources.UsuarioAlumnoResource, '/alumno/<id>')
+
+    api.add_resource(resources.UsuariosProfesoresResource, '/profesor/<id>')
+
+    api.add_resource(resources.ClasesProfesoresResource, '/clases')
+
+    api.add_resource(resources.LoginResource, '/login')
+    
+    api.add_resource(resources.PagoResource, '/pagos/<id>')
+
+    api.add_resource(resources.PlanificacionAlumnoResource, '/planificacion_alum/<id>')
+
+    api.add_resource(resources.PlanificacionProfesoresResource, '/planificacion_prof/<id>')
+    
+    api.add_resource(resources.PlanificacionesProfesoresResource, '/planificaciones')
+    
     #Cargar la aplicacion en la API de Flask Restful
     #es para que la aplicacion de flask funcione como API
     api.init_app(app)
