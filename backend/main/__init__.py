8c63e578aec9aa4cd6d332683c1920b520230337
tomasgrau2 <t.grau@alumno.um.edu.ar>
2023-04-19 19:58:35 -0300

    cambios

--- backend/main/__init__.py
+++ backend/main/__init__.py
@@ -1,14 +1,21 @@
 from flask import Flask
 from dotenv import load_dotenv
+import os
 
 #Importamos nuevas librerias clase 3
 from flask_restful import Api #Agrego la clase Api
+
+#Importar SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 #Importo dir de recursos 
 import main.resources as resources
 
 #Inicio Restful
 api = Api()
+
+#Inicializar SQLAlchemy
+db = SQLAlchemy()
 
 #Metodo que inicializa la app
 def create_app():
@@ -17,6 +24,11 @@ def create_app():
 
     #variables de entorno
     load_dotenv()
+
+    # Si no existe el archivo de base de datos, crearlo (solo válido si se utiliza sqlite)
+    if not os.path.exists(os.getenv('DATABASE_PATH')+os.getenv('DATABASE_NAME')):
+        os.mknod(os.getenv('DATABASE_PATH')+os.getenv('DATABASE_NAME'))
+
     #cargar a la API el recurso Animales y especificar la ruta 
     api.add_resource(resources.AnimalesResource, '/animales')
     #cargar a la API el recurso Animal y especificar la ruta 
@@ -44,6 +56,11 @@ def create_app():
     
     api.add_resource(resources.PlanificacionesProfesoresResource, '/planificaciones')
     
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    #Url de configuracion de base de datos
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////'+os.getenv('DATABASE_PATH')+os.getenv('DATABASE_NAME')
+    db.init_app(app)
+
     #Cargar la aplicacion en la API de Flask Restful
     #es para que la aplicacion de flask funcione como API
     api.init_app(app)
